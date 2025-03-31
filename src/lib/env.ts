20cd13d04ce1563a13b2fb36ad30ff410ac468b8
Camilo Romero <ca.romerom1@uniandes.edu.co>
2025-03-30 23:12:15 -0500

    chore: include env vars config

---
+++ src/lib/env.ts
@@ -0,0 +1,18 @@
+import { type } from "arktype";
+
+const envType = type({
+  BETTER_AUTH_SECRET: "string",
+  BETTER_AUTH_URL: "string",
+  DATABASE_URL: "string",
+  "PORT?": "number",
+});
+
+const parsedEnv = envType(process.env);
+
+if (parsedEnv instanceof type.errors) {
+  console.error("Invalid environment variables:");
+  console.error(parsedEnv);
+  process.exit(1);
+}
+
+export const env = parsedEnv;
